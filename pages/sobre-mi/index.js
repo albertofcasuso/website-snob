859c66934955ffc5bcbca891c878aad8dac4d9b4
Casuso <hi@casuso.dev>
2022-07-08 19:47:17 +0200

    inicio de sobre mi

--- pages/sobre-mi/index.js
+++ pages/sobre-mi/index.js
@@ -1,7 +1,35 @@
+import InstagramLink from '../../components/InstagramLink'
+
 function SobreMi (){
     return (
-        <div className="container">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi neque eros, feugiat nec porttitor aliquet, congue id quam. Phasellus at diam leo. Praesent sed euismod tellus. Sed ornare felis purus, venenatis fermentum lacus gravida eu. Maecenas a nisi id arcu luctus finibus. Ut vulputate, elit non rutrum rhoncus, leo dui suscipit ex, vitae interdum elit elit ac nisl. Phasellus efficitur libero tristique eros tempor, eu ultrices justo imperdiet. Sed mauris metus, aliquet sed euismod sagittis, finibus a lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut laoreet enim, non rhoncus orci. Praesent vitae lacus a massa tincidunt consequat eu eu nunc.</p>
+        <div className="container-sobremi">
+            <div className="container-sobremi__title">
+                <h3>PREMIOS / CONFERENCIAS / PRENSA</h3>
+            </div>
+            <hr/>
+            <div className="container-sobremi__personal">
+            <div className="container-sobremi__personal-texto">
+                <h3>MAZDEY SNOB</h3>
+                <InstagramLink/>
+            </div>
+                <div className="container-sobremi__personal-texto">
+                    <p>Nacida en México y actualmente viviendo en España. Mazdey estudio la Licenciatura en
+Diseño Gráfico en la Universidad de Guadalajara, aunque siempre ha tenido interés por el cine
+y la realización cinematográfica. A través del fashion film, ha explorado el lenguaje audiovisual
+creando historias que reflejan su propia visión del mundo.</p>
+                    <p>Mazdey también es la fundadora de
+Snob Solutions, una productora audiovisual independiente que se enfoca en proyectos de
+moda, arte, música y estilo de vida.</p>
+                    <p>Con más de siete años de experiencia en la producción de
+video para artistas, diseñadores y marcas de moda, varias universidades y plataformas la han
+contratado para hablar sobre el fashion film como género a través de su experiencia como
+directora y este año fue invitada</p>
+                </div>
+            </div>
+            <hr/>
+            <div className="container-sobremi__premios">
+                
+            </div>
         </div>
     )
 }
