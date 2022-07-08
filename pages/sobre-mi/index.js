492738136f9af62f55737a701fef2b0cd74b544d
Casuso <hi@casuso.dev>
2022-07-08 20:39:28 +0200

    premios

--- pages/sobre-mi/index.js
+++ pages/sobre-mi/index.js
@@ -37,6 +37,80 @@ function SobreMi() {
       </div>
       <hr />
       <div className="container-sobremi__premios">
+        <p>
+          <span className="negritas">1First place People's Choice Winner </span>
+          ASVOFF 13 (París, Francia, 2021)
+        </p>
+        <p>
+          <span className="negritas">Phenomenal Woman in Film Award</span> -
+          Cinema Femme Short Film Festival (Chicago, 2022)
+        </p>
+        <p>
+          <span className="negritas"> Best Foreign Fashion Film Winner </span> -
+          Digital media Fest (Roma, Italia, 2021)
+        </p>
+        <p>
+          <span className="negritas"> 2 Place Best Fashion Film Winner </span>-
+          Fash Film Fete (Phoenix, 2021)
+        </p>
+        <p>
+          <span className="negritas"> Best Glam Winner </span> -
+          LAFashionFestival, (Los Angeles California, 2020)
+        </p>
+        <p>
+          <span className="negritas"> Best Emerging Talent Nominee </span> -
+          London Fashion Film Festival, (2021)
+        </p>
+        <p>
+          <span className="negritas"> Top 10 Best Production Design </span> -
+          Berlin Commercial (2021)
+        </p>
+        <p>
+          <span className="negritas"> Best Accessory Design Nominee </span> -
+          Sarajevo Fashion Film Festival (2020)
+        </p>
+        <p>
+          <span className="negritas"> Best LGBTQ -short Winner </span> -
+          Emberlight International Film Festival (Michigan, 2021)
+        </p>
+        <p>
+          <span className="negritas"> Best Fashion Story Winner </span> - Panama
+          Fashion Film Festival (2018)
+        </p>
+        <p>
+          <span className="negritas"> Mejor styling Winner </span> - México
+          Fashion Film Festival (2017)
+        </p>
+        <p>
+          <span className="negritas"> Beauty prize Winner </span> - ASVOFF MX
+          (2014)
+        </p>
+        <p>
+          <span className="negritas"> GRRL HAUS Cinema </span> - Berlin (2018),
+          (2021)
+        </p>
+        GRRL HAUS TELEPHONE (2021)
+        <p>
+          <span className="negritas"> Femme Frontera Filmmaker Showcase</span>
+          (El Paso, Texas, 2021)
+        </p>
+        <p>
+          <span className="negritas">
+            Canadian International Fashion Film Festival
+          </span>
+          (2016), (2017), (2018) y (2020) (2021)
+        </p>
+        <p>
+          <span className="negritas"> Fashion Film Festival Milano </span>
+          (2017) (2021)
+        </p>
+        <p>
+          <span className="negritas">Berlin Fashion Film Festival </span> (2017)
+          (2021)
+        </p>
+        <p>
+          <span className="negritas">Girona Film Festival </span> (2021)
+        </p>
       </div>
     </div>
   );
