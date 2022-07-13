e12ad65952f20fabcc0ab4baa1d6c9a45a3d172d
Casuso <albertofcasuso@gmail.com>
2022-07-13 11:22:00 +0200

    firebase performance

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/style.scss";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getPerformance } from "firebase/performance";
 import { useEffect } from "react";
 
 function App({ Component, pageProps }) {
@@ -19,6 +20,7 @@ function App({ Component, pageProps }) {
     if (process.env.NODE_ENV === 'production') {
       const app = initializeApp(firebaseConfig);
       const analytics = getAnalytics(app);
+      const perf = getPerformance(app);
     }
   }, [])
   return <Component {...pageProps} />;
