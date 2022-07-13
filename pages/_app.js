84bc6cacd1361b5e3073ae2293a43f8216d097ca
Casuso <albertofcasuso@gmail.com>
2022-07-13 11:11:05 +0200

    updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,26 @@
 import "../styles/style.scss";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { useEffect } from "react";
 
 function App({ Component, pageProps }) {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCwd54H2r-19ayJy1EaSZNf6F2eDweH7fc",
+    authDomain: "web-snob.firebaseapp.com",
+    projectId: "web-snob",
+    storageBucket: "web-snob.appspot.com",
+    messagingSenderId: "116537960046",
+    appId: "1:116537960046:web:d2d9a07f4da1965dbd287f",
+    measurementId: "G-S8ELMEWDGC"
+  };
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      const app = initializeApp(firebaseConfig);
+      const analytics = getAnalytics(app);
+    }
+  }, [])
   return <Component {...pageProps} />;
 }
 
