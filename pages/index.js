c2f3b3491e34327dc3d4b7df4acabede53acfafa
Casuso <albertofcasuso@gmail.com>
2022-07-13 11:01:19 +0200

    firebase install, npm update

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 import Head from "next/head";
 import Cover from "../components/Image";
 import Text from "../components/Text";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 
 export default function Home() {
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyCwd54H2r-19ayJy1EaSZNf6F2eDweH7fc",
+      authDomain: "web-snob.firebaseapp.com",
+      projectId: "web-snob",
+      storageBucket: "web-snob.appspot.com",
+      messagingSenderId: "116537960046",
+      appId: "1:116537960046:web:d2d9a07f4da1965dbd287f",
+      measurementId: "G-S8ELMEWDGC"
+    };
+  
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
   return (
     <div>
       <Head>
@@ -13,17 +28,7 @@ export default function Home() {
         <script src="https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js"></script>
         <script src="https://www.gstatic.com/firebasejs/9.9.0/firebase-analytics.js"></script>
         <script>
-          const firebaseConfig = {
-          apiKey: "AIzaSyCwd54H2r-19ayJy1EaSZNf6F2eDweH7fc",
-          authDomain: "web-snob.firebaseapp.com",
-          projectId: "web-snob",
-          storageBucket: "web-snob.appspot.com",
-          messagingSenderId: "116537960046",
-          appId: "1:116537960046:web:d2d9a07f4da1965dbd287f",
-          measurementId: "G-S8ELMEWDGC"
-          };
-          const app = initializeApp(firebaseConfig);
-          const analytics = getAnalytics(app);
+          
         </script>
       </Head>
       <div className="container">
