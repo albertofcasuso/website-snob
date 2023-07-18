8b2551e12f4150ac06db8e949b09b870f11b514f
Casuso <albertofcasuso@gmail.com>
2023-07-18 11:40:09 +0200

    new design added

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,12 @@
-import "../styles/style.scss";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { useEffect } from "react";
+import "../node_modules/react-modal-video/css/modal-video.css";
+import "../public/css/glitch.css";
+import "../public/css/plugins.css";
+// import "../public/css/style-update.css";
+import "../public/css/style.css";
+import "../styles/globals.css";
 
-function App({ Component, pageProps }) {
-  const firebaseConfig = {
-    apiKey: "AIzaSyCwd54H2r-19ayJy1EaSZNf6F2eDweH7fc",
-    authDomain: "web-snob.firebaseapp.com",
-    projectId: "web-snob",
-    storageBucket: "web-snob.appspot.com",
-    messagingSenderId: "116537960046",
-    appId: "1:116537960046:web:d2d9a07f4da1965dbd287f",
-    measurementId: "G-S8ELMEWDGC",
-  };
-
-  useEffect(() => {
-    if (process.env.NODE_ENV === "production") {
-      const app = initializeApp(firebaseConfig);
-      const analytics = getAnalytics(app);
-    }
-  }, []);
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-export default App;
+export default MyApp;
