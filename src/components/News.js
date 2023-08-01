e57a40adbfd06dc6a4a6453e0f0d8637b8029450
Casuso <albertofcasuso@gmail.com>
2023-08-01 13:14:13 +0200

    added news

--- src/components/News.js
+++ src/components/News.js
@@ -1,129 +1,71 @@
-import { Fragment, useEffect, useState } from "react";
-import Modal from "react-modal";
-import { CloseButton } from "../plugin/svg";
+import Link from "next/link";
+const newsData = [
+  {
+    img: "img/news/1.jpg",
+    date: "August 9, 2021",
+    title: '"Mijo" ID- Italy Among the 21 FASHION FILMS FAVS by the Vice team.',
+    link: "https://i-d.vice.com/it/article/epxbqm/migliori-fashion-film-2021",
+  },
+  {
+    img: "img/news/1.jpg",
+    date: "August 9, 2021",
+    title: "Directors Notes Interview",
+    link: "https://directorsnotes.com/2022/02/15/mazdey-snob-mijo",
+  },
+  {
+    img: "img/news/1.jpg",
+    date: "August 9, 2021",
+    title: "Neo2 Magazine - “Estreno de Mijo” ",
+    link: "https://www.neo2.com/fashion-film-mijo-mazdey-snob-mexico-anos-40-pachucos",
+  },
+  {
+    img: "img/news/1.jpg",
+    date: "August 9, 2021",
+    title:
+      "Interview: “Mijo” director Mazdey Snob, 2022 recipient of the Cinema Femme Short Film Fest Phenomenal Woman in Film award",
+    link: "https://cinemafemme.com/2022/05/20/interview-mijo-director-mazdey-snob-2022-recipient-of-the-cinema-femme-short-film-fest-phenomenal-woman-in-film-award/",
+  },
+  {
+    img: "img/news/1.jpg",
+    date: "August 9, 2021",
+    title: "Bubblegum Club - Johannesburg, South Africa",
+    link: "https://bubblegumclub.co.za/fashion/mijo-a-colourful-film-inspired-by-pachucos-fashion",
+  },
+  {
+    img: "img/news/1.jpg",
+    date: "August 9, 2021",
+    title: "Kaltblut Magazine Mijo – An Ode to Mexican Fashion of the 40s",
+    link: "https://www.kaltblut-magazine.com/mijo-an-ode-to-mexican-fashion-of-the-40s/",
+  },
+  {
+    img: "img/news/1.jpg",
+    date: "August 9, 2021",
+    title: "GIRLS IN FILMS",
+    link: "https://www.girlsinfilm.net/videos/mijo",
+  },
+  {
+    img: "img/news/1.jpg",
+    date: "August 9, 2021",
+    title: "Entrevista con Mazdey, El Fashion Film para el #futuromoda ",
+    link: "https://www.kavolta.com/2020/07/mazdey-snob-solutions-fashion-film-kavoltalive",
+  },
+  {
+    img: "img/news/1.jpg",
+    date: "August 9, 2021",
+    title: "Entrevista / Snob Solutions Fashion Film ",
+    link: "https://www.kavolta.com/2015/03/entrevista-snob-solutions",
+  },
+];
+
 const News = ({ ActiveIndex, animation }) => {
-  const [isOpen4, setIsOpen4] = useState(false);
-  const [modalContent, setModalContent] = useState({});
-
-  useEffect(() => {
-    var lists = document.querySelectorAll(".news_list > ul > li");
-    let box = document.querySelector(".cavani_fn_moving_box");
-    if (!box) {
-      let body = document.querySelector("body");
-      let div = document.createElement("div");
-      div.classList.add("cavani_fn_moving_box");
-      body.appendChild(div);
-    }
-
-    lists.forEach((list) => {
-      list.addEventListener("mouseenter", (event) => {
-        box.classList.add("opened");
-        var imgURL = list.getAttribute("data-img");
-        box.style.backgroundImage = `url(${imgURL})`;
-        box.style.top = event.clientY - 50 + "px";
-        // console.log(event.clientY);
-        if (imgURL === "") {
-          box.classList.remove("opened");
-          return false;
-        }
-      });
-      list.addEventListener("mouseleave", () => {
-        box.classList.remove("opened");
-      });
-    });
-  }, []);
-
-  function toggleModalFour(value) {
-    setIsOpen4(!isOpen4);
-    setModalContent(value);
-  }
-  const newsData = [
-    {
-      img: "img/news/1.jpg",
-      tag: "Branding",
-      date: "August 9, 2021",
-      comments: "3 comments",
-      title: "Reasons Why People Use Discord",
-      text1:
-        "Cavani is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.",
-      text2:
-        "In today's digital world, your website is the first interaction consumers have with your business. That's why almost 95 percent of a user's first impression relates to web design. It's also why web design services can have an immense impact on your company's bottom line.",
-      text3:
-        "That's why more companies are not only reevaluating their website's design but also partnering with Kura, the web design agency that's driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we're confident we can design a custom website that drives sales for your unique business.",
-    },
-    {
-      img: "img/news/2.jpg",
-      tag: "Branding",
-      date: "August 9, 2021",
-      comments: "3 comments",
-      title: "Why We Should Read Fewer Books",
-      text1:
-        "Cavani is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.",
-      text2:
-        "In today's digital world, your website is the first interaction consumers have with your business. That's why almost 95 percent of a user's first impression relates to web design. It's also why web design services can have an immense impact on your company's bottom line.",
-      text3:
-        "That's why more companies are not only reevaluating their website's design but also partnering with Kura, the web design agency that's driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we're confident we can design a custom website that drives sales for your unique business.",
-    },
-    {
-      img: "img/news/3.jpg",
-      tag: "Branding",
-      date: "August 9, 2021",
-      comments: "3 comments",
-      title: "A Final Farewell to the iPod",
-      text1:
-        "Cavani is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.",
-      text2:
-        "In today's digital world, your website is the first interaction consumers have with your business. That's why almost 95 percent of a user's first impression relates to web design. It's also why web design services can have an immense impact on your company's bottom line.",
-      text3:
-        "That's why more companies are not only reevaluating their website's design but also partnering with Kura, the web design agency that's driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we're confident we can design a custom website that drives sales for your unique business.",
-    },
-    {
-      img: "img/news/4.jpg",
-      tag: "Branding",
-      date: "August 9, 2021",
-      comments: "3 comments",
-      title: "Dealing with Spring Allergy Symptoms",
-      text1:
-        "Cavani is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.",
-      text2:
-        "In today's digital world, your website is the first interaction consumers have with your business. That's why almost 95 percent of a user's first impression relates to web design. It's also why web design services can have an immense impact on your company's bottom line.",
-      text3:
-        "That's why more companies are not only reevaluating their website's design but also partnering with Kura, the web design agency that's driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we're confident we can design a custom website that drives sales for your unique business.",
-    },
-    {
-      img: "img/news/5.jpg",
-      tag: "Branding",
-      date: "August 9, 2021",
-      comments: "3 comments",
-      title: "How to Take a Break from Yourself",
-      text1:
-        "Cavani is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.",
-      text2:
-        "In today's digital world, your website is the first interaction consumers have with your business. That's why almost 95 percent of a user's first impression relates to web design. It's also why web design services can have an immense impact on your company's bottom line.",
-      text3:
-        "That's why more companies are not only reevaluating their website's design but also partnering with Kura, the web design agency that's driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we're confident we can design a custom website that drives sales for your unique business.",
-    },
-    {
-      img: "img/news/6.jpg",
-      tag: "Branding",
-      date: "August 9, 2021",
-      comments: "3 comments",
-      title: "The Age of Extinction Is Here",
-      text1:
-        "Cavani is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.",
-      text2:
-        "In today's digital world, your website is the first interaction consumers have with your business. That's why almost 95 percent of a user's first impression relates to web design. It's also why web design services can have an immense impact on your company's bottom line.",
-      text3:
-        "That's why more companies are not only reevaluating their website's design but also partnering with Kura, the web design agency that's driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we're confident we can design a custom website that drives sales for your unique business.",
-    },
-  ];
   return (
-    <Fragment>
+    <>
       <div
         className={
           ActiveIndex === 3
-            ? `cavani_tm_section active animated ${animation ? animation : "fadeInUp"
-            }`
+            ? `cavani_tm_section active animated ${
+                animation ? animation : "fadeInUp"
+              }`
             : "cavani_tm_section hidden animated"
         }
         id="news__"
@@ -138,41 +80,20 @@ const News = ({ ActiveIndex, animation }) => {
                 {newsData.map((news, i) => (
                   <li data-img={`img/news/${i + 1}.jpg`} key={i}>
                     <div className="list_inner">
-                      <span className="number">{`${i <= 9 ? 0 : ""}${i + 1
-                        }`}</span>
+                      <span className="number">{`${i <= 9 ? 0 : ""}${
+                        i + 1
+                      }`}</span>
                       <div className="details">
-                        <div className="extra_metas">
-                          <ul>
-                            <li>
-                              <span>{news.date}</span>
-                            </li>
-                            <li>
-                              <span>
-                                <a
-                                  href="#"
-                                  onClick={() => toggleModalFour(news)}
-                                >
-                                  Branding
-                                </a>
-                              </span>
-                            </li>
-                            <li>
-                              <span>
-                                <a
-                                  href="#"
-                                  onClick={() => toggleModalFour(news)}
-                                >
-                                  0 Comments
-                                </a>
-                              </span>
-                            </li>
-                          </ul>
-                        </div>
+                        <div className="extra_metas"></div>
                         <div className="post_title">
                           <h3>
-                            <a href="#" onClick={() => toggleModalFour(news)}>
+                            <Link
+                              href={news.link}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
                               {news.title}
-                            </a>
+                            </Link>
                           </h3>
                         </div>
                       </div>
@@ -184,57 +105,7 @@ const News = ({ ActiveIndex, animation }) => {
           </div>
         </div>
       </div>
-      {modalContent && (
-        <Modal
-          isOpen={isOpen4}
-          onRequestClose={toggleModalFour}
-          contentLabel="My dialog"
-          className="mymodal"
-          overlayClassName="myoverlay"
-          closeTimeoutMS={300}
-          openTimeoutMS={300}
-        >
-          <div className="cavani_tm_modalbox opened">
-            <div className="box_inner">
-              <div className="close" onClick={toggleModalFour}>
-                <a href="#">
-                  <i className="icon-cancel"></i>
-                </a>
-              </div>
-              <div className="description_wrap">
-                <div className="news_popup_informations">
-                  <div className="image">
-                    <img src="img/thumbs/4-2.jpg" alt="" />
-                    <div
-                      className="main"
-                      data-img-url="img/news/1.jpg"
-                      style={{ backgroundImage: `url(${modalContent.img})` }}
-                    />
-                  </div>
-                  <div className="details">
-                    <div className="meta">
-                      <ul>
-                        <li><span>{modalContent.date}</span></li>
-                        <li><span><a href="#">{modalContent.tag}</a></span></li>
-                        <li><span><a href="#">{modalContent.comments}</a></span></li>
-                      </ul>
-                    </div>
-                    <div className="title">
-                      <h3>{modalContent.title}</h3>
-                    </div>
-                  </div>
-                  <div className="text">
-                    <p>{modalContent.text1}</p>
-                    <p>{modalContent.text2}</p>
-                    <p>{modalContent.text3}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Modal>
-      )}
-    </Fragment>
+    </>
   );
 };
 export default News;
