19a8fcf74aeee13a0eecfd0a63d73bd842b09112
Casuso <albertofcasuso@gmail.com>
2023-08-01 13:29:48 +0200

    new config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  distDir: "build",
+  // distDir: "build",
+  output: "export",
   images: {
     unoptimized: true,
   },
